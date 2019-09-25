4f520e67950b7fe9763acad1ebb5adb07717d864
tifeb <tifebakre@yahoo.co.uk>
2019-09-26 00:28:35 +0100

    Implemented: LogIn Activity

---
+++ app/src/main/java/com/fade/sharedclipboard/LoginActivity.java
@@ -0,0 +1,26 @@
+package com.fade.sharedclipboard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class LoginActivity extends AppCompatActivity {
+
+	//TODO:	Setup basic sign up then firebase based.
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_login);
+
+	}
+
+	public void signUpClicked(View view){
+
+	}
+
+	public void signInClicked(View view){
+
+	}
+}
